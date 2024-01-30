6fe0d8d70dfe1f4f4562500517488f9f348caf05
Alena <92329824+poletala@users.noreply.github.com>
2024-01-30 22:02:41 +0500

    task 6

---
+++ CodewarsArray6.js
@@ -0,0 +1,10 @@
+const initialArray = ['Say', 'hello', 'to', 'world']
+function firstLetters (arr) {
+    return arr.reduce((acc, value, index) => {
+        const first = value[0]
+        acc = acc + first
+        return acc
+    }, '')
+}
+const result = firstLetters(initialArray) 
+console.log(Array.from(result)) 
