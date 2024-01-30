edb81fd4912ce433ce3839b7f03e0f37077ba025
Alena <92329824+poletala@users.noreply.github.com>
2024-01-30 22:37:07 +0500

    deleted tasks

--- CodewarsArray2.js
+++
@@ -1,6 +0,0 @@
-const initialArray = ['Say', 'hello', 'to', 'world', 'javascript', 'students']
-function longerThanFive (arr) {
-    return arr.filter(element => typeof element === 'string' && element.length > 5)
-}
-const result = longerThanFive(initialArray) 
-console.log(result) 
