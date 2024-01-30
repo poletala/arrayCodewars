0c85de8e2da06f05f108a99dc3e67e64c9bb0fcf
Alena <92329824+poletala@users.noreply.github.com>
2024-01-30 17:02:17 +0500

    task 2

---
+++ CodewarsArray2.js
@@ -0,0 +1,6 @@
+const initialArray = ['Say', 'hello', 'to', 'world', 'javascript', 'students']
+function longerThanFive (arr) {
+    return arr.filter(element => typeof element === 'string' && element.length > 5)
+}
+const result = longerThanFive(initialArray) 
+console.log(result) 
