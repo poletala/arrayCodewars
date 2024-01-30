22ef76c7c05aa985af89e8e523cffe72b637ce45
Alena <92329824+poletala@users.noreply.github.com>
2024-01-30 16:13:04 +0500

    1

---
+++ CodewarsArray1.js
@@ -0,0 +1,6 @@
+const initialArray = [8, 0, true, null, undefined, , 7, 44, 9]
+function evenNumbersOnly (arr) {
+  return arr.filter(element => (typeof element === 'number') && (element % 2 == 0))
+}
+const result = evenNumbersOnly(initialArray)
+console.log(result)
