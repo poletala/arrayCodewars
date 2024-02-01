6545e67a0e49085fd9f11da2c3372791cc7cb4d7
Alena <92329824+poletala@users.noreply.github.com>
2024-02-01 19:00:43 +0500

    task 7 v2

--- CodewarsArray7.js
+++ CodewarsArray7.js
@@ -1,59 +1,39 @@
 const initialArray = [
-	{ "name": "Viktor", "age": true },
+	{ "name": "Viktor", "age": 24 },
 	{ "name": "Aboba", "age": 16 },
 	{ "name": "Abiba", "age": 35 },
 ]
 
 //                                ПЕРВЫЙ ВАРИАНТ
 
-//              Сначала проверка на соответствие age типу number
-
-let arrayAge = initialArray.map(a => a.age);
-function ageCheck (arr) {
-    for (let i = 0; i < arr.length; i++) {
-		if (typeof arr[i] === 'number') {
-			return arr
-		} else {
-			console.error('Age must be a number')
-		}
-	}
-}
-ageCheck(arrayAge)
-
-//                           Сортировка массива
-
-function sortByAge (arr) {
-    let arrNew = arr.sort((a,b) => {
-        return a.age - b.age
-    })
-    return arrNew
-}
-const result = sortByAge(initialArray) 
-console.log(result) 
+// function sortByAge (arr) {
+//     let arrNew = arr.sort((a,b) => {
+//         return a.age - b.age
+//     })
+//     return arrNew
+// }
+// const result = sortByAge(initialArray) 
+// console.log(result) 
 
 
 
 //                                    ВТОРОЙ ВАРИАНТ
 
 
-// function sortByAge (arr) {
-// let arrayAge = arr.map(a => a.age);
-//     for (let i = 0; i < arr.length; i++) {
-// 		if (typeof arrayAge[i] === 'number') {
-// 			let arrNew = arr.sort((a,b) => {
-//                 return a.age - b.age
-//             })
-//             return arrNew
-// 		} else {
-// 			console.error('Age must be a number')
-// 		}
-// 	}
-// }
+function sortByAge (arr) {
+let arrayAge = arr.map(a => a.age);
+    for (let i = 0; i < arr.length; i++) {
+		if (typeof arrayAge[i] === 'number') {
+			let arrNew = arr.sort((a,b) => {
+                return a.age - b.age
+            })
+            return arrNew
+		} else {
+			console.error('Age must be a number')
+            return
+		}
+	}
+}
+const result = sortByAge(initialArray) 
+console.log(result) 
 
-// const result = sortByAge(initialArray) 
-// console.log(result) 
-
-// Но во втором варианте он все равно выводит и ошибку, и массив
-// помогите
-// как сделать так, чтобы не выводил массив в случае ошибки? 
-// как упростить написание проверки age на соответствию типу number
