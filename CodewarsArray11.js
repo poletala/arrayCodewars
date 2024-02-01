55e639aea96e5834dcc79fa73b773515bc9ef51d
Alena <92329824+poletala@users.noreply.github.com>
2024-02-01 22:25:45 +0500

    task 11 v2

--- CodewarsArray11.js
+++ CodewarsArray11.js
@@ -1,26 +1,5 @@
 const initialArray = ['Say', 'hello', 'to', 'World']
 
-// function firstcaseOnly (arr) {
-//     for (let i = 0; i < arr.length; i++) {
-//         if (typeof arr[i] === 'string') {
-//             return arr.reduce((acc, value, index) => {
-//                 const first = value[0];
-//                 acc = acc + first; 
-//                 return acc}, '')
-//         }
-//          else {return}
-//     }
-// }
-// const stringFirst = firstcaseOnly(initialArray) 
-
-// function uppercaseOnly(str){
-//     return  str.match(/[A-Z]/g, ""); 
-// }
-// const result = uppercaseOnly(stringFirst)
-// console.log(result) 
-
-
-
 function uppercaseOnly (arr) {
     for (let i = 0; i < arr.length; i++) {
         if (typeof arr[i] === 'string') {
