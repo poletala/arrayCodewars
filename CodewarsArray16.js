6647758a2ad1910bb41d813d825f0a6b08d511fc
Alena <92329824+poletala@users.noreply.github.com>
2024-02-04 01:26:09 +0500

    task 16

---
+++ CodewarsArray16.js
@@ -0,0 +1,16 @@
+const initialArray = [
+	{ "name": "Viktor", "age": 24 },
+	{ "name": "Aboba", "age": 16 },
+	{ "name": "Abiba", "age": 35 },
+	{ "name": "Viktor", "age": 35 },
+	{ "name": "Abiba", "age": 35 },
+]
+
+function unique (arr) {
+    const allNames = arr.map(obj => obj.name);
+    let uniqueNames = allNames.filter((item, i, arr) => arr.indexOf(item) !== i);
+    return initialArray.filter(initialArrayItem => !uniqueNames.includes(initialArrayItem.name))
+}
+
+console.log(unique(initialArray))
+
