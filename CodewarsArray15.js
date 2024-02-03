d09656e019a4abb04b6a1909d7b1300bdeb02384
Alena <92329824+poletala@users.noreply.github.com>
2024-02-03 23:16:43 +0500

    task 15

---
+++ CodewarsArray15.js
@@ -0,0 +1,6 @@
+const initialArray = ['js', 'world', 'viktor', 'awdjsdc', 'javes']
+function contains (arr) {
+return arr.filter(str => str.includes('js')); 
+}
+const result = contains(initialArray) 
+console.log(result) 
