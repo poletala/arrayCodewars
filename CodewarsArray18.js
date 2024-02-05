1997e9cbb41e09655108ebdd877636f9482f3401
Alena <92329824+poletala@users.noreply.github.com>
2024-02-06 02:24:25 +0500

    task 18

---
+++ CodewarsArray18.js
@@ -0,0 +1,32 @@
+const initialArray1 = [
+	{ "name": "Viktor", "age": 24 },
+	{ "name": "Abiba", "age": 35 },
+	{ "name": "Viktor", "age": 35 git status},
+	{ "name": "Abiba", "age": 35 },
+]
+
+const initialArray2 = [
+	{ "name": "Viktor", "age": 24 },
+	{ "name": "Aboba", "age": 16 },
+	{ "name": "Abiba", "age": 35 },
+]
+
+function unique (arr1, arr2) {
+    
+    let arrNew = []
+    for (let i=0;i<arr1.length;i++)  {
+        for (let j=0;j<arr2.length;j++) {
+            const stringifiedObj1 = JSON.stringify(arr1[i]);
+            const stringifiedObj2 = JSON.stringify(arr2[j]);
+            if (stringifiedObj1 === stringifiedObj2) {
+                arrNew.push(arr2[j])
+            }
+        }
+    }
+    return arrNew
+}
+const result = unique(initialArray1, initialArray2) 
+console.log(result)
+
+// я сдаюсь
+
