894fc19db67e475fc94ab72354101d1a127a68c7
Alena <92329824+poletala@users.noreply.github.com>
2024-02-05 21:48:40 +0500

    task 19 v2

--- CodewarsArray19.js
+++ CodewarsArray19.js
@@ -1,15 +1,4 @@
 const initialArray = ['Say', 'привет', 'to', 'АБЦ Wold']
-function latinAlphabetOnly (arr) {
-    newArr = []
-    for (let i=0; i<arr.length;i++) {
-        if (arr[i].match(/[a-z]/i)) {
-         return newArr.push(arr[i])
-        }
-        else {
-            console.error('Error')
-        }
-    }
-}
 
 function latinAlphabetOnly (arr) {
     const filtered = arr.filter(samarr => samarr.match(/[а-я\W|_]/i));
@@ -17,14 +6,5 @@ function latinAlphabetOnly (arr) {
 }
 
 const result = latinAlphabetOnly(initialArray) 
-
 console.log(result) 
 
-
-
-// const sampleArray = ['Say', 'привет', 'to_', 'АБЦ Wold'];
-// const filtered = sampleArray.filter(samarr => samarr.match(/[а-я\W|_]/i));
-// console.log(filtered);
-// let difference = sampleArray.filter((item) => 
-// !filtered.includes(item))
-// console.log(difference)
