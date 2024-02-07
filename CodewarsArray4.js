4e431c66fc0133ca3cba75ac82a59f82630bc1b1
Alena <92329824+poletala@users.noreply.github.com>
2024-02-07 15:09:05 +0500

    Update task 4
    delete repeating lines

--- CodewarsArray4.js
+++ CodewarsArray4.js
@@ -1,20 +1,18 @@
 const initialArray1 = [1, 2, 3]
 const initialArray2 = [4, 5, 6]
+//          V1
 const result = initialArray1.concat(initialArray2)
 console.log(result)
 
 
-const initialArray1 = [1, 2, 3]
-const initialArray2 = [4, 5, 6]
-Array.prototype.push.apply(initialArray1, initialArray2);
-console.log(initialArray1)
+//         V2
+// Array.prototype.push.apply(initialArray1, initialArray2);
+// console.log(initialArray1)
 
-const initialArray1 = [1, 2, 3]
-const initialArray2 = [4, 5, 6]
-initialArray1.push(...initialArray2)
-console.log(initialArray1)
+//          V3
+// initialArray1.push(...initialArray2)
+// console.log(initialArray1)
 
-const initialArray1 = [1, 2, 3]
-const initialArray2 = [4, 5, 6]
-const all = [...initialArray1, ...initialArray2];  
-console.log(all)
+//          V4
+// const all = [...initialArray1, ...initialArray2];  
+// console.log(all)
