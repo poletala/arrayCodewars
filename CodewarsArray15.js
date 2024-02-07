76afd14cb94a28b32fa5aa68f1e6ecf9ea064f60
Alena <92329824+poletala@users.noreply.github.com>
2024-02-07 15:37:19 +0500

    Update task 15
    delete spaces

--- CodewarsArray15.js
+++ CodewarsArray15.js
@@ -1,6 +1,6 @@
 const initialArray = ['js', 'world', 'viktor', 'awdjsdc', 'javes']
-function contains (arr) {
-return arr.filter(str => str.includes('js')); 
+function contains(arr) {
+  return arr.filter(str => str.includes('js')); 
 }
 const result = contains(initialArray) 
 console.log(result) 
