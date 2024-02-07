c97df1480cb1371256dba802a4b5ded8650efedd
Alena <92329824+poletala@users.noreply.github.com>
2024-02-07 19:03:12 +0500

    Update
    delete checking the element type

--- CodewarsArray10.js
+++ CodewarsArray10.js
@@ -1,14 +1,8 @@
 const initialArray = [1, 2, 3, 2, 6, 5, 4, 4, 9, 1]
 
 function uniqueOnly(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (typeof arr[i] === 'number') {
-        	return Array.from(new Set(arr))
-        	} else {
-        	    return
-        	}
-        }
-}
+    return Array.from(new Set(arr))
+} 
 
 const result = uniqueOnly(initialArray) 
 console.log(result) 
