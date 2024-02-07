565a513c746df574305fc77f3b962cd01d3f6c32
Alena <92329824+poletala@users.noreply.github.com>
2024-02-07 19:17:01 +0500

    Update
    the data type checking has been deleted

--- CodewarsArray13.js
+++ CodewarsArray13.js
@@ -3,15 +3,7 @@ const average = array => array.reduce((a, b) => a + b) / array.length;
 const a = average(initialArray)
 
 function greaterThanAverage(arr,average) {
-    for (let i = 0; i < arr.length; i++) {
-        if (typeof arr[i] === 'number') {
-            return arr.filter(item => (item >= a));
-          }
-        else {
-            console.error(`Element '${arr[i]}' is not a number`)
-            return
-        }
-    }
+     return arr.filter(item => (item >= a));
 }
 const result = greaterThanAverage(initialArray, a)
 console.log(result) 
