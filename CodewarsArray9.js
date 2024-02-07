c4b5db87ab1c1a1fe38dd1bb0534c9d0b66d6516
Alena <92329824+poletala@users.noreply.github.com>
2024-02-07 15:24:53 +0500

    Update task 9
    delete spaces and v1

--- CodewarsArray9.js
+++ CodewarsArray9.js
@@ -1,26 +1,13 @@
 const initialArray = [1, 2, 3, 4]
- 
-//                                   Первый вариант
-function multiplyByTwo (arr) {
-    return arr.map((value, index) => {
-    return value * 2
-})
+function multiplyByTwo(arr) {
+    for (let i = 0; i < arr.length; i++) {
+		if (typeof arr[i] === 'number') {
+			return  arr.map((value,index) =>  2 * value)
+		} else {
+   console.error(`Element '${arr[i]}' must be a number`)
+		}
+	}
 }
-
-//                                     Второй вариант 
-
-// Почему не работает проверка на тип данных?
-
-// function multiplyByTwo (arr) {
-//     for (let i = 0; i < arr.length; i++) {
-// 		if (typeof arr[i] === 'number') {
-// 			return  arr.map((value,index) =>  2 * value)
-// 		} else {
-// 			return
-// 		}
-// 	}
-// }
-
 const result = multiplyByTwo(initialArray)
 console.log(result) 
 
