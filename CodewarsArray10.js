2a899a03a11597fabfad786b42c1e7e84f176948
Alena <92329824+poletala@users.noreply.github.com>
2024-02-07 15:26:43 +0500

    Update task 10
    delete spaces

--- CodewarsArray10.js
+++ CodewarsArray10.js
@@ -1,6 +1,6 @@
 const initialArray = [1, 2, 3, 2, 6, 5, 4, 4, 9, 1]
 
-function uniqueOnly (arr) {
+function uniqueOnly(arr) {
     for (let i = 0; i < arr.length; i++) {
         if (typeof arr[i] === 'number') {
         	return Array.from(new Set(arr))
