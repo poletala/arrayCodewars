73b369a8f2d616fcf4dd6494796b17e00da1132a
Alena <92329824+poletala@users.noreply.github.com>
2024-02-07 18:55:13 +0500

    Updated
    checking the element type

--- CodewarsArray9.js
+++ CodewarsArray9.js
@@ -1,13 +1,19 @@
 const initialArray = [1, 2, 3, 4]
+
 function multiplyByTwo(arr) {
+    const newArr = [];
     for (let i = 0; i < arr.length; i++) {
-		if (typeof arr[i] === 'number') {
-			return  arr.map((value,index) =>  2 * value)
-		} else {
-   console.error(`Element '${arr[i]}' must be a number`)
-		}
-	}
+    if (typeof arr[i] === 'number') {
+      newArr.push(arr[i] * 2)
+    } 
+    if (typeof arr[i] === 'boolean' || typeof arr[i] === 'string' ||
+     typeof arr[i] === 'null' || typeof arr[i] === 'undefined') {
+        console.error(`Element '${arr[i]}' must be a number`)
+        return
+    }
+  } return newArr
 }
+
 const result = multiplyByTwo(initialArray)
 console.log(result) 
 
