94c0bf2a1166676afdb929ca35bcfafbd11a58c8
Alena <92329824+poletala@users.noreply.github.com>
2024-02-07 19:19:45 +0500

    Update
    the data type checking has been deleted

--- CodewarsArray8.js
+++ CodewarsArray8.js
@@ -1,13 +1,6 @@
 const initialArray = [true, 2, 3, 4, 5]
 function sum(arr) {
-    for (let i = 0; i < arr.length; i++) {
-		if (typeof arr[i] === 'number') {
-			return  arr.reduce((acc, value) => acc + value)
-		} else {
-			console.error(`Element '${arr[i]}' must be a number`)
-			return
-		}
-	}
+   return  arr.reduce((acc, value) => acc + value)
 }
 const result = sum(initialArray) 
 console.log(result) 
