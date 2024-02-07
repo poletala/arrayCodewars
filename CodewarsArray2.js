eb72de475648849a6c65be230bdb9f4a23228a33
Alena <92329824+poletala@users.noreply.github.com>
2024-02-07 15:04:45 +0500

    Update task 2
    delete spaces

--- CodewarsArray2.js
+++ CodewarsArray2.js
@@ -1,5 +1,5 @@
 const initialArray = ['Say', 'hello', 'to', 'world', 'javascript', 'students']
-function longerThanFive (arr) {
+function longerThanFive(arr) {
     return arr.filter(element => typeof element === 'string' && element.length > 5)
 }
 const result = longerThanFive(initialArray) 
