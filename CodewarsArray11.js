b07046a8b6c959db9c2de801de18417611c7efc5
Alena <92329824+poletala@users.noreply.github.com>
2024-02-07 19:10:01 +0500

    Update
    data type checking has been deleted

--- CodewarsArray11.js
+++ CodewarsArray11.js
@@ -1,15 +1,11 @@
 const initialArray = ['Say', 'hello', 'to', 'World']
 
 function uppercaseOnly(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (typeof arr[i] === 'string') {
-            let string = arr.reduce((acc, value, index) => {
-                const first = value[0];
-                acc = acc + first; 
-                return acc}, '')
-            return string.match(/[A-Z]/g, "");
-        }
-         else {return}
-    }
+    let string = arr.reduce((acc, value, index) => {
+        const first = value[0];
+        acc = acc + first; 
+        return acc}, '')
+        return string.match(/[A-Z]/g, "");
 }
+        
 console.log(uppercaseOnly(initialArray))
